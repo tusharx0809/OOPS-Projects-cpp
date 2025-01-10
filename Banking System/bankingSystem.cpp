6d3f6bd5b6bf45968f60aa9d6aa6d5bd4dbd40bb
tusharx0809 <tusharbhatia0809@outlook.com>
2025-01-10 18:11:37 +0530

    started: started bank class in banking system

--- Banking System/bankingSystem.cpp
+++ Banking System/bankingSystem.cpp
@@ -1,55 +1,80 @@
-#include<iostream>
-#include<stdio.h>
-#include<stdlib.h>
-#include<string>
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
 using namespace std;
 
-class Account{
+class Account
+{
 
-    private:
-        int accountNumber;
-        string name;
-        double balance;
-        string accountType;
+private:
+    int accountNumber;
+    string name;
+    double balance;
+    string accountType;
 
-    public:
-
-        //Constructor
-        Account(int accNum, string accName, double initialBalance, string accType)
+public:
+    // Constructor
+    Account(int accNum, string accName, double initialBalance, string accType)
         : accountNumber(accNum), name(accName), balance(initialBalance), accountType(accType) {}
 
-        //Methods
-        void deposit(double amount){
-            balance += amount;
-            cout << "Deposited: "<< amount << ". New Balance: " << balance << endl;
-        }
+    // Methods
+    void deposit(double amount)
+    {
+        balance += amount;
+        cout << "Deposited: " << amount << ". New Balance: " << balance << endl;
+    }
 
-        bool withdraw(double amount){
-            if(amount > balance){
-                cout<<"Insufficient Balance!"<<endl;
-                return false;
+    bool withdraw(double amount)
+    {
+        if (amount > balance)
+        {
+            cout << "Insufficient Balance!" << endl;
+            return false;
+        }
+        balance -= amount;
+        cout << "Withdrawn: " << amount << ". Remaining Balance: " << balance;
+        << endl;
+        return true;
+    }
+
+    void displayDetails() const
+    {
+        cout << "Account number: " << accountNumber << endl;
+        cout << "Name: " << name << endl;
+        cout << "Balance: " << balance << endl;
+        cout << "Account Type: " << accountType << endl;
+    }
+
+    int getAccountNumber() const
+    {
+        return accountNumber;
+    }
+
+    double getBalance() const
+    {
+        return balance;
+    }
+
+    void setBalance(double newBalance)
+    {
+        balance = newBalance;
+    }
+};
+
+class Bank
+{
+private:
+    vector<Account> accounts;
+    Account *findAccount(int accountNumber)
+    {
+        for (auto &account : accounts)
+        {
+            if (account.getAccountNumber() == accountNumber)
+            {
+                return &account;
             }
-            balance -= amount;
-            cout<<"Withdrawn: "<<amount<<". Remaining Balance: "<<balance;<<endl;
-            return true;
         }
-
-        void displayDetails() const {
-            cout<<"Account number: "<< accountNumber << endl;
-            cout<<"Name: "<< name << endl;
-            cout<<"Balance: "<<balance<<endl;
-            cout<<"Account Type: "<<accountType << endl;
-        }
-
-        int getAccountNumber() const{
-            return accountNumber;
-        }
-
-        double getBalance() const{
-            return balance;
-        }
-
-        void setBalance(double newBalance){
-            balance = newBalance;
-        }
+        return NULL;
+    }
 };
