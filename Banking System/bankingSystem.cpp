ca365de7b823b9dca824634a79242e865e94e599
tusharx0809 <tusharbhatia0809@outlook.com>
2025-01-10 17:17:45 +0530

    added: added account class in banking system

--- Banking System/bankingSystem.cpp
+++ Banking System/bankingSystem.cpp
@@ -17,4 +17,39 @@ class Account{
         //Constructor
         Account(int accNum, string accName, double initialBalance, string accType)
         : accountNumber(accNum), name(accName), balance(initialBalance), accountType(accType) {}
+
+        //Methods
+        void deposit(double amount){
+            balance += amount;
+            cout << "Deposited: "<< amount << ". New Balance: " << balance << endl;
+        }
+
+        bool withdraw(double amount){
+            if(amount > balance){
+                cout<<"Insufficient Balance!"<<endl;
+                return false;
+            }
+            balance -= amount;
+            cout<<"Withdrawn: "<<amount<<". Remaining Balance: "<<balance;<<endl;
+            return true;
+        }
+
+        void displayDetails() const {
+            cout<<"Account number: "<< accountNumber << endl;
+            cout<<"Name: "<< name << endl;
+            cout<<"Balance: "<<balance<<endl;
+            cout<<"Account Type: "<<accountType << endl;
+        }
+
+        int getAccountNumber() const{
+            return accountNumber;
+        }
+
+        double getBalance() const{
+            return balance;
+        }
+
+        void setBalance(double newBalance){
+            balance = newBalance;
+        }
 };
