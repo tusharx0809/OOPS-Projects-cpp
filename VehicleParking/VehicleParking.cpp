f9baab059ac796fc2fb548775a1e186d4aa54f4a
tusharx0809 <tusharbhatia0809@outlook.com>
2025-02-01 18:12:03 +0530

    added: added parking class in vehicle parking

--- VehicleParking/VehicleParking.cpp
+++ VehicleParking/VehicleParking.cpp
@@ -1,7 +1,18 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+class Parking{
+    private:
+        int floors;
+        int numberOfParking;
+        vector<vector<unordered_map<int, vector<string>>> parkingLot;
 
+    public:
+        Parking(int floors, int numberOfParking) : floors(floors), numberOfParking(numberOfParking) {
+            parkingLot.resize(floors, vector<unordered_map<int, vector<string>>>(numberOfParking));
+        }
+
+};
 
 int main(){
     return 0;
