4bdac5771136c5e82d604b730dd863b748b77788
tusharx0809 <tusharbhatia0809@outlook.com>
2025-02-02 09:04:20 +0530

    park vehcicle functionality fixed

--- VehicleParking/VehicleParking.cpp
+++ VehicleParking/VehicleParking.cpp
@@ -1,52 +1,65 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-class Parking{
-    private:
-        int floors; //number of floors in multistorey parking
-        int numberOfParking; //number of parkings on one storey
-        vector<vector<pair<string, string>>> parkingLot; //2d vector which represents multistorey parking, internal unordered map represents parking number and its value representing a vector with license plate and time entered in parking
+class Parking
+{
+private:
+    int floors;                                      // number of floors in multistorey parking
+    int numberOfParking;                             // number of parkings on one storey
+    vector<vector<pair<string, string>>> parkingLot; // 2d vector which represents multistorey parking, internal unordered map represents parking number and its value representing a vector with license plate and time entered in parking
 
-        /*  Structure of parking lot
-            parkingLot = {
-                floor 0: [
-                    parking spot 0 -> {"ABC123","10:00AM"}
-                    parking spot 1 -> {}
-                    parking spot 2 -> {}
-                ],
-                floor 1: [
-                    parking spot 0 -> {}
-                    parking spot 1 -> {}
-                    parking spot 2 -> {}
-                ]
+    /*  Structure of parking lot
+        parkingLot = {
+            floor 0: [
+                parking spot 0 -> {"ABC123","10:00AM"}
+                parking spot 1 -> {}
+                parking spot 2 -> {}
+            ],
+            floor 1: [
+                parking spot 0 -> {}
+                parking spot 1 -> {}
+                parking spot 2 -> {}
+            ]
+        }
+    */
+
+public:
+    Parking(int floors, int numberOfParking) : floors(floors), numberOfParking(numberOfParking)
+    {
+        parkingLot.resize(floors, vector<pair<string, string>>(numberOfParking, {"", ""}));
+    }
+
+    bool parkVehicle(const string &licensePlate, const string &time)
+    {
+        int spot = 0;
+        int floor = 0;
+
+        while(floor < floors){
+            if(parkingLot[floor][spot].first == ""){
+                parkingLot[floor][spot] = {licensePlate, time};
+                cout<<"Car Parked at floor:"<<floor<<" and spot: "<<spot<<endl;
+                return true;
             }
-        */
-
-    public:
-        Parking(int floors, int numberOfParking) : floors(floors), numberOfParking(numberOfParking) {
-            parkingLot.resize(floors, vector<pair<string, string>>(numberOfParking, {"", ""}));
+            spot++;
+            if(spot == numberOfParking){
+                floor++;
+                spot = 0;
+            }
         }
 
-        bool parkVehicle(int floor, int spot, const string& licensePlate, const string& time){
-            if(floor < 0 || floor >=floors || spot < 0 || spot >=numberOfParking){
-                cout<<"Invalid floor or spot!"<<endl;
-                return false;
-            }
-            if(parkingLot[floor][spot].first != ""){
-                cout<<"Parking already occupied!"<<endl;
-                return false;
-            }
-            parkingLot[floor][spot] = {licensePlate, time};
-            cout<<"Vehicle parked at floor "<<floor<<" ,spot "<<spot<<endl;
-            return true;
-        }
+        cout<<"Parking Full!"<<endl;
+        return false;
+    }
 
-           bool removeVehicle(int floor, int spot) {
-        if (floor < 0 || floor >= floors || spot < 0 || spot >= numberOfParking) {
+    bool removeVehicle(int floor, int spot)
+    {
+        if (floor < 0 || floor >= floors || spot < 0 || spot >= numberOfParking)
+        {
             cout << "Invalid floor or spot number." << endl;
             return false;
         }
-        if (parkingLot[floor][spot].first == "") {
+        if (parkingLot[floor][spot].first == "")
+        {
             cout << "Parking spot is already empty." << endl;
             return false;
         }
@@ -56,14 +69,20 @@ class Parking{
     }
 
     // Function to display the parking lot status
-    void displayParkingLot() {
-        for (int i = 0; i < floors; ++i) {
+    void displayParkingLot()
+    {
+        for (int i = 0; i < floors; ++i)
+        {
             cout << "Floor " << i << ":" << endl;
-            for (int j = 0; j < numberOfParking; ++j) {
+            for (int j = 0; j < numberOfParking; ++j)
+            {
                 cout << "  Spot " << j << ": ";
-                if (parkingLot[i][j].first == "") {
+                if (parkingLot[i][j].first == "")
+                {
                     cout << "Empty";
-                } else {
+                }
+                else
+                {
                     cout << "Vehicle " << parkingLot[i][j].first << " entered at " << parkingLot[i][j].second;
                 }
                 cout << endl;
@@ -72,14 +91,16 @@ class Parking{
     }
 };
 
-int main() {
-    Parking parking(2, 3); // 2 floors, 3 parking spots per floor
+int main()
+{
+    Parking parking(2, 2); // 2 floors, 3 parking spots per floor
 
-    parking.parkVehicle(0, 0, "ABC123", "10:00AM");
-    parking.parkVehicle(1, 1, "DEF456", "11:30AM");
-    parking.displayParkingLot();
-
-    parking.removeVehicle(0, 0);
+    parking.parkVehicle("ABC123", "10:00AM");
+    parking.parkVehicle("DEF456", "11:30AM");
+    parking.parkVehicle("QWE345","12:00PM");
+    parking.parkVehicle("QWT345","12:30PM");
+    parking.removeVehicle(0,1);
+    parking.parkVehicle("ERT456","1:00PM");
     parking.displayParkingLot();
 
     return 0;
