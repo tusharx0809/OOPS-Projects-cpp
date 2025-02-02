b61b39bafda3ff741b60e2f7cb610473fb1324c8
tusharx0809 <tusharbhatia0809@outlook.com>
2025-02-02 19:21:10 +0530

    vehicles types added and datatype converted to tuple

--- VehicleParking/VehicleParking.cpp
+++ VehicleParking/VehicleParking.cpp
@@ -4,38 +4,23 @@ using namespace std;
 class Parking
 {
 private:
-    int floors;                                      // number of floors in multistorey parking
-    int numberOfParking;                             // number of parkings on one storey
-    vector<vector<pair<string, string>>> parkingLot; // 2d vector which represents multistorey parking, pair represents license plate and time at which the customer parked the vehicle
-
-    /*  Structure of parking lot
-        parkingLot = {
-            floor 0: [
-                parking spot 0 -> {"ABC123","10:00AM"}
-                parking spot 1 -> {}
-                parking spot 2 -> {}
-            ],
-            floor 1: [
-                parking spot 0 -> {}
-                parking spot 1 -> {}
-                parking spot 2 -> {}
-            ]
-        }
-    */
+    int floors;                                               // number of floors in multistorey parking
+    int numberOfParking;                                      // number of parkings on one storey
+    vector<vector<tuple<string, string, string>>> parkingLot; // 2d vector which represents multistorey parking, tuple represents license plate and time at which the customer parked the vehicle
 
 public:
     Parking(int floors, int numberOfParking) : floors(floors), numberOfParking(numberOfParking)
     {
-        parkingLot.resize(floors, vector<pair<string, string>>(numberOfParking, {"", ""}));
+        parkingLot.resize(floors, vector<tuple<string, string, string>>(numberOfParking, make_tuple("", "", "")));
         loadParkingLotFromFile("parking_lot.txt");
     }
 
-    //function to get currentDateTime
+    // function to get currentDateTime
     string getCurrentDateTime()
     {
         time_t timestamp;
         time(&timestamp);
-        
+
         struct tm *local_time = localtime(&timestamp);
 
         int year = local_time->tm_year + 1900; // tm_year is years since 1900
@@ -43,55 +28,56 @@ public:
         int day = local_time->tm_mday;         // tm_mday is day of the month (1-31)
         int hour = local_time->tm_hour;        // tm_hour is hour (0-23)
         int minute = local_time->tm_min;       // tm_min is minute (0-59)
-        int second = local_time->tm_sec;       // tm_sec is second (0-59)
 
         string dateTime = to_string(month) + "-" + to_string(day) + "-" + to_string(year) + "," +
-                      (hour < 10 ? "0" + to_string(hour) : to_string(hour)) + ":" +
-                      (minute < 10 ? "0" + to_string(minute) : to_string(minute));
+                          (hour < 10 ? "0" + to_string(hour) : to_string(hour)) + ":" +
+                          (minute < 10 ? "0" + to_string(minute) : to_string(minute));
 
         return dateTime;
     }
 
-    //function to get the parking duration of a vehicle
-    long int getParkingTime(const string &parkingDateTime,const string &exitDateTime){
+    // function to get the parking duration of a vehicle
+    long int getParkingTime(const string &parkingDateTime, const string &exitDateTime)
+    {
         string format = "%m-%d-%Y,%H:%M";
 
         tm tm1 = {};
         tm tm2 = {};
 
-        istringstream ss1(exitDateTime);
-        ss1 >> get_time(&tm1, format.c_str());
-
-        istringstream ss2(parkingDateTime);
+        istringstream ss2(exitDateTime);
         ss2 >> get_time(&tm2, format.c_str());
+
+        istringstream ss1(parkingDateTime);
+        ss1 >> get_time(&tm1, format.c_str());
 
         auto tp1 = chrono::system_clock::from_time_t(mktime(&tm1));
         auto tp2 = chrono::system_clock::from_time_t(mktime(&tm2));
 
-        long int parkingTime = chrono::duration_cast<chrono::minutes>(tp2 - tp1).count();
+        long int parkingTime = chrono::duration_cast<chrono::minutes>(tp2- tp1).count();
 
         return parkingTime;
     }
 
-    //Function to calculae parking charges per minute
-    double calculateParkingCharges(int minutes){
-        return minutes * 0.15;
+    // Function to calculate parking charges per minute
+    double calculateParkingCharges(int minutes, const string &type)
+    {
+        return (type == "Car") ? minutes * 0.15 : minutes * 0.075;
     }
 
-    //Function to add a vehicle at first empty parking spot
-    bool parkVehicle(const string &licensePlate)
+    // Function to add a vehicle at the first empty parking spot
+    bool parkVehicle(const string &licensePlate, const string &type)
     {
         int spot = 0;
         int floor = 0;
 
         while (floor < floors)
         {
-            if (parkingLot[floor][spot].first == "")
+            if (get<0>(parkingLot[floor][spot]) == "")
             {
                 string parkingDateTime = getCurrentDateTime();
-                parkingLot[floor][spot] = {licensePlate, parkingDateTime};
+                parkingLot[floor][spot] = make_tuple(licensePlate, type, parkingDateTime);
                 saveParkingLotToFile("parking_lot.txt");
-                cout << "Car Parked at floor:" << floor << " and spot: " << spot << endl;
+                cout << type << " parked at floor: " << floor << " and spot: " << spot << endl;
                 displayParkingLot();
                 return true;
             }
@@ -108,7 +94,7 @@ public:
         return false;
     }
 
-    //Function to remove Vehicle from parking
+    // Function to remove Vehicle from parking
     bool removeVehicle(const string &licensePlate)
     {
         int spot = 0;
@@ -116,17 +102,18 @@ public:
 
         while (floor < floors)
         {
-            if (parkingLot[floor][spot].first == licensePlate)
+            string storedLicensePlate = get<0>(parkingLot[floor][spot]);
+            if (!storedLicensePlate.empty() && storedLicensePlate == licensePlate)
             {
                 string exitDateTime = getCurrentDateTime();
-                long int totalParkingTime = getParkingTime(exitDateTime, parkingLot[floor][spot].second);
-                double charges = calculateParkingCharges(totalParkingTime);
+                long int totalParkingTime = getParkingTime(get<2>(parkingLot[floor][spot]), exitDateTime);
+                double charges = calculateParkingCharges(totalParkingTime, get<1>(parkingLot[floor][spot]));
 
-                parkingLot[floor][spot] = {"", ""};
-                cout << "Car: " << licensePlate << " exited from floor:" << floor << " and spot: " << spot <<" at: "<<exitDateTime<<" Charges: "<< charges << endl;
+                parkingLot[floor][spot] = make_tuple("", "", "");
+                cout << get<1>(parkingLot[floor][spot]) << " " << licensePlate << " exited from floor:" << floor << " and spot: " << spot << " at: " << exitDateTime << " Charges: " << charges << endl;
                 saveParkingLotToFile("parking_lot.txt");
-                displayParkingLot();
-                logChargesToFile(licensePlate, exitDateTime, totalParkingTime, charges,"charges.txt");
+                displayParkingLot();  // Now called only once
+                logChargesToFile(licensePlate, exitDateTime, totalParkingTime, charges, "charges.txt");
                 return true;
             }
             spot++;
@@ -142,7 +129,6 @@ public:
         return false;
     }
 
-
     // Function to display the parking lot status
     void displayParkingLot()
     {
@@ -152,100 +138,134 @@ public:
             for (int j = 0; j < numberOfParking; ++j)
             {
                 cout << "  Spot " << j << ": ";
-                if (parkingLot[i][j].first == "")
+                if (get<0>(parkingLot[i][j]) == "")
                 {
                     cout << "Empty";
                 }
                 else
                 {
-                    cout << "Vehicle " << parkingLot[i][j].first << " entered at " << parkingLot[i][j].second;
+                    cout << "Vehicle " << get<0>(parkingLot[i][j])
+                         << " type " << get<1>(parkingLot[i][j])
+                         << " entered at " << get<2>(parkingLot[i][j]);
                 }
                 cout << endl;
             }
         }
     }
 
-    //Function to log charges info in txt file when vehicle exits from parking
-    void logChargesToFile(const string & licensePlate,const string &exitDateTime, long int duration, double charges, const string &filename){
+    // Function to log charges info in txt file when vehicle exits from parking
+    void logChargesToFile(const string &licensePlate, const string &exitDateTime, long int duration, double charges, const string &filename)
+    {
         ofstream file(filename, ios::app);
 
-        if(!file.is_open()){
-            cout<<"Error opening file!"<<endl;
+        if (!file.is_open())
+        {
+            cout << "Error opening file!" << endl;
             return;
         }
-        file<<licensePlate<<" "<<exitDateTime<<" "<<duration<<" "<<charges<<endl;
-        file.close(); 
+        file << licensePlate << " " << exitDateTime << " " << duration << " " << charges << endl;
+        file.close();
     }
 
-    //Function to save vehicle info in txt file when vehicle is entered into the parking lot
-    void saveParkingLotToFile(const string &filename){
+    // Function to save vehicle info in txt file when vehicle is entered into the parking lot
+    void saveParkingLotToFile(const string &filename)
+    {
         ofstream file(filename);
 
-        if(!file.is_open()){
-            cout<<"Error Opening File!"<<endl;
+        if (!file.is_open())
+        {
+            cout << "Error Opening File!" << endl;
             return;
         }
 
-        for(int i=0;i<floors;i++){
-            file << "Floor "<<i<<":"<<endl;
-            for(int j=0;j<numberOfParking;j++){
-                file<<"  Spot "<<j<<": ";
-                if(parkingLot[i][j].first == ""){
-                    file<<"Empty"<<endl;
-                }else{
-                    file<<"Vehicle "<<parkingLot[i][j].first <<" entered at "<<parkingLot[i][j].second << endl;
+        for (int i = 0; i < floors; i++)
+        {
+            file << "Floor " << i << ":" << endl;
+            for (int j = 0; j < numberOfParking; j++)
+            {
+                file << "  Spot " << j << ": ";
+                if (get<0>(parkingLot[i][j]) == "")
+                {
+                    file << "Empty" << endl;
+                }
+                else
+                {
+                    file << "Vehicle " << get<0>(parkingLot[i][j]) << " type " << get<1>(parkingLot[i][j]) << " entered at " << get<2>(parkingLot[i][j]) << endl;
                 }
             }
         }
         file.close();
-        cout<<"Parking Lot Saved to "<<filename<<endl;
+        cout << "Parking Lot Saved to " << filename << endl;
     }
 
-    //Function to load the parking lot txt file when main function is executed
-    void loadParkingLotFromFile(const string &filename){
-        ifstream file(filename);
+    // Function to load the parking lot txt file when the main function is executed
+    void loadParkingLotFromFile(const string &filename)
+{
+    ifstream file(filename);
 
-        if(!file.is_open()){
-            cout<<"Error Opening File!"<<endl;
-            return;
+    if (!file.is_open())
+    {
+        cout << "Error Opening File!" << endl;
+        return;
+    }
+
+    string line;
+    int floor = -1;
+    int spot = -1;
+
+    while (getline(file, line))
+    {
+        // Detect the floor based on the "Floor" keyword
+        if (line.find("Floor") != string::npos)
+        {
+            floor++;
+            spot = -1;  // Reset spot index when a new floor is encountered
         }
+        // Detect the spot based on the "Spot" keyword
+        else if (line.find("Spot") != string::npos)
+        {
+            spot++;
+            size_t pos = line.find("Vehicle ");
+            if (pos != string::npos)
+            {
+                size_t time_pos = line.find(" entered at ");
+                size_t type_pos = line.find(" type ") + 6; // Fix the position for type field
 
-        string line;
-        int floor = -1;
-        int spot = -1;
+                string licensePlate = line.substr(pos + 8, type_pos - (pos + 8) - 6);  // Extract license plate
+                string type = line.substr(type_pos, time_pos - type_pos);               // Extract vehicle type
+                string time = line.substr(time_pos + 12);  // Extract time, adjusted by " entered at "
 
-        while(getline(file, line)){
-            if(line.find("Floor") != string::npos){
-                floor++;
-                spot = -1;
-            }else if(line.find("Spot") != string::npos){
-                spot++;
-                size_t pos = line.find("Vehicle");
-                if(pos != string::npos){
-                    size_t time_pos = line.find(" entered at ");
-                    string licensePlate = line.substr(pos+8, time_pos - (pos+8));
-                    string time = line.substr(time_pos + 12);
-
-                    parkingLot[floor][spot] = {licensePlate, time};
-                }else{
-                    parkingLot[floor][spot] = {"",""};
+                // Check if license plate, type, or time are empty or invalid
+                if (!licensePlate.empty() && !type.empty() && !time.empty())
+                {
+                    // Store all information in parkingLot tuple
+                    parkingLot[floor][spot] = make_tuple(licensePlate, type, time);
+                }
+                else
+                {
+                    // If data is incomplete, mark as empty
+                    parkingLot[floor][spot] = make_tuple("", "", "");
                 }
             }
+            else
+            {
+                // If no vehicle is found, mark the spot as empty
+                parkingLot[floor][spot] = make_tuple("", "", "");
+            }
         }
-        file.close();
-        cout<<"Parking lot loaded from "<<filename<<endl;
     }
+}
+
 };
-
 
 int main()
 {
-    Parking parking(2, 5); 
+    Parking parking(2, 5);
 
-    // Vehicle will be parked wherever it finds the first empty spot parking
-    parking.removeVehicle("PMN234");
-    parking.removeVehicle("ABCD1234");
-    parking.removeVehicle("LIN234");
-    
+    // Park some vehicles
+    parking.parkVehicle("DEF234","Car");
+
+    // Vehicle will be removed
+
     return 0;
 }
